42cf4bb0bcb2af7f2e12943646ef4ddc76bd4e97
shimotaroo <shimotaro.0327@gmail.com>
2022-01-25 22:41:37 +0900

    feat: Controllerの雛形を作成

--- api/app/Http/Controllers/LoginController.php
+++ api/app/Http/Controllers/LoginController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class LoginController extends Controller
 {
-    //
+    /**
+     * ログイン
+     * @param LoginRequest $request
+     * @return JsonResource
+     */
+    public function login(LoginRequest $request): JsonResource
+    {
+        // 処理
+    }
 }
--- api/app/Http/Controllers/MemoController.php
+++ api/app/Http/Controllers/MemoController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\MemoPostRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class MemoController extends Controller
 {
-    //
+    /**
+     * メモの全件取得
+     * @return AnonymousResourceCollection
+     */
+    public function fetch(): AnonymousResourceCollection
+    {
+        // 処理
+    }
+
+    /**
+     * メモの登録
+     * @param MemoPostRequest $request
+     * @return JsonResponse
+     */
+    public function create(MemoPostRequest $request): JsonResponse
+    {
+        // 処理
+    }
 }
