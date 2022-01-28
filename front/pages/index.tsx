bce041495b3e3f9d55bc44868b3c6e7a0cc67361
shimotaroo <shimotaro.0327@gmail.com>
2022-01-28 22:49:03 +0900

    feat: フロントエンドの実装

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,6 +1,9 @@
 import type { NextPage } from 'next';
-import { useState } from 'react';
+import { AxiosError, AxiosResponse } from 'axios';
+import { ChangeEvent, useState } from 'react';
 import { RequiredMark } from '../components/RequiredMark';
+import { axiosApi } from '../lib/axios';
+import { useRouter } from 'next/router';
 
 // POSTデータの型
 type LoginForm = {
@@ -12,6 +15,8 @@ type LoginForm = {
 type Validation = LoginForm & { loginFailed: string };
 
 const Home: NextPage = () => {
+  // ルーター定義
+  const router = useRouter();
   // state定義
   const [loginForm, setLoginForm] = useState<LoginForm>({
     email: '',
@@ -22,6 +27,30 @@ const Home: NextPage = () => {
     password: '',
     loginFailed: '',
   });
+
+  // POSTデータの更新
+  const updateLoginForm = (e: ChangeEvent<HTMLInputElement>) => {
+    setLoginForm({ ...loginForm, [e.target.name]: e.target.value });
+  };
+
+  // ログイン
+  const login = () => {
+    axiosApi
+      // CSRF保護の初期化
+      .get('/sanctum/csrf-cookie')
+      .then((res) => {
+        // ログイン処理
+        axiosApi
+          .post('/login', loginForm)
+          .then((response: AxiosResponse) => {
+            console.log(response.data);
+            router.push('/memos');
+          })
+          .catch((err: AxiosError) => {
+            console.log(err.response);
+          });
+      });
+  };
 
   return (
     <div className='w-2/3 mx-auto py-24'>
@@ -35,6 +64,8 @@ const Home: NextPage = () => {
           <input
             className='p-2 border rounded-md w-full outline-none'
             name='email'
+            value={loginForm.email}
+            onChange={updateLoginForm}
           />
           {/* <p className='py-3 text-red-500'>必須入力です。</p> */}
         </div>
@@ -50,6 +81,8 @@ const Home: NextPage = () => {
             className='p-2 border rounded-md w-full outline-none'
             name='password'
             type='password'
+            value={loginForm.password}
+            onChange={updateLoginForm}
           />
           {/* <p className='py-3 text-red-500'>
             8文字以上の半角英数字で入力してください。
@@ -59,7 +92,10 @@ const Home: NextPage = () => {
           {/* <p className='py-3 text-red-500'>
             IDまたはパスワードが間違っています。
           </p> */}
-          <button className='bg-gray-700 text-gray-50 py-3 sm:px-20 px-10 rounded-xl cursor-pointer drop-shadow-md hover:bg-gray-600'>
+          <button
+            className='bg-gray-700 text-gray-50 py-3 sm:px-20 px-10 rounded-xl cursor-pointer drop-shadow-md hover:bg-gray-600'
+            onClick={login}
+          >
             ログイン
           </button>
         </div>
