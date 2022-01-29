9edc99b842c5b9f151f58285b32cd6eb9e54c284
shimotaroo <shimotaro.0327@gmail.com>
2022-01-30 01:13:27 +0900

    feat: フロントエンド実装

--- front/pages/memos/post.tsx
+++ front/pages/memos/post.tsx
@@ -1,7 +1,55 @@
+import { AxiosError, AxiosResponse } from 'axios';
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { ChangeEvent, useState } from 'react';
 import { RequiredMark } from '../../components/RequiredMark';
+import { axiosApi } from '../../lib/axios';
+
+// POSTデータの型
+type MemoForm = {
+  title: string;
+  body: string;
+};
 
 const Post: NextPage = () => {
+  // ルーター定義
+  const router = useRouter();
+  // state定義
+  const [memoForm, setMemoForm] = useState<MemoForm>({
+    title: '',
+    body: '',
+  });
+  const [validation, setValidation] = useState<MemoForm>({
+    title: '',
+    body: '',
+  });
+
+  // POSTデータの更新
+  const updateMemoForm = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setMemoForm({ ...memoForm, [e.target.name]: e.target.value });
+  };
+
+  // メモの登録
+  const createMemo = () => {
+    axiosApi
+      // CSRF保護の初期化
+      .get('/sanctum/csrf-cookie')
+      .then((res) => {
+        // APIへのリクエスト
+        axiosApi
+          .post('/api/memos', memoForm)
+          .then((response: AxiosResponse) => {
+            console.log(response.data);
+            router.push('/memos');
+          })
+          .catch((err: AxiosError) => {
+            console.log(err.response);
+          });
+      });
+  };
+
   return (
     <div className='w-2/3 mx-auto'>
       <div className='w-1/2 mx-auto mt-32 border-2 px-12 py-16 rounded-2xl'>
@@ -14,6 +62,8 @@ const Post: NextPage = () => {
           <input
             className='p-2 border rounded-md w-full outline-none'
             name='title'
+            value={memoForm.title}
+            onChange={updateMemoForm}
           />
         </div>
         <div className='mb-5'>
@@ -26,10 +76,15 @@ const Post: NextPage = () => {
             name='body'
             cols={30}
             rows={4}
+            value={memoForm.body}
+            onChange={updateMemoForm}
           />
         </div>
         <div className='text-center'>
-          <button className='bg-gray-700 text-gray-50 py-3 sm:px-20 px-10 mt-8 rounded-xl cursor-pointer drop-shadow-md hover:bg-gray-600'>
+          <button
+            className='bg-gray-700 text-gray-50 py-3 sm:px-20 px-10 mt-8 rounded-xl cursor-pointer drop-shadow-md hover:bg-gray-600'
+            onClick={createMemo}
+          >
             登録する
           </button>
         </div>
